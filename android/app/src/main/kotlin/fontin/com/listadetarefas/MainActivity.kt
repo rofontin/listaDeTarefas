683ae3cfcccf62b4941f9b0c5ff0f3065aa9e9da
Riquelmy <fontinriquelmy@gmail.com>
2020-06-20 23:50:11 -0300

    first

---
+++ android/app/src/main/kotlin/fontin/com/listadetarefas/MainActivity.kt
@@ -0,0 +1,6 @@
+package fontin.com.listadetarefas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
