3d22bb2c877ad9cc6513837891df5c92b78eaf27
Riquelmy <fontinriquelmy@gmail.com>
2020-07-05 01:58:13 -0300

    finalização da lista

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,23 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      _toDoList.sort((a, b){
+        if(a["ok"] && !b["ok"]) return 1;
+        else if(!a["ok"] && b["ok"]) return -1;
+        else return 0;
+
+      });
+
+      _saveData();
+    });
+
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,10 +96,13 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
               padding: EdgeInsets.only(top: 10.0),
               itemCount: _toDoList.length,
               itemBuilder: buildItem),
+            )
           )
         ],
       ),
@@ -135,6 +155,7 @@ class _HomeState extends State<Home> {
           duration: Duration(seconds: 10),
         );
 
+        Scaffold.of(context).removeCurrentSnackBar();
         Scaffold.of(context).showSnackBar(snack);
         });
       },
